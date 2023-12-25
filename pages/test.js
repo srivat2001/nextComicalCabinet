2d5acde918c7159d46054ee36cef9141ec2ba9ed
Srivathsan G <77399329+srivat2001@users.noreply.github.com>
2023-12-25 15:10:45 +0530

    Update test.js
    Updated test files

--- pages/test.js
+++ pages/test.js
@@ -1,50 +1,6 @@
-import { useState } from "react";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { storage } from "../util/js/firebaseconn";
+
 const FileUploader = () => {
-  const [file, setFile] = useState(null);
-
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const handleUpload = async () => {
-    if (file) {
-      const storage = getStorage();
-      const storageRef = ref(storage, `images/${file.name}`);
-
-      // Generate a random ID for metadata
-      const randomId = Math.random().toString(36).substring(7);
-
-      // Set file metadata
-      const metadata = {
-        contentType: file.type,
-        customMetadata: {
-          id: randomId,
-        },
-      };
-
-      try {
-        // Upload the file and metadata
-        await uploadBytes(storageRef, file, metadata);
-
-        // Get the download URL
-        const downloadURL = await getDownloadURL(storageRef);
-
-        console.log("File uploaded successfully!");
-        console.log("Download URL:", downloadURL);
-      } catch (error) {
-        console.error("Error uploading file:", error.message);
-      }
-    }
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload File</button>
-    </div>
-  );
+  return <div>Test Works</div>
 };
 
 export default FileUploader;
